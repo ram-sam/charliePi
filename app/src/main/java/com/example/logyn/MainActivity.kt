3868f85ef7eddd59105220106461b9521bba1f91
Samantha Souza <samanthaesouza@outlook.com>
2024-09-12 20:23:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logyn/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.logyn
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import android.util.Log
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            blockLogin()
+        }
+    }
+
+    private fun blockLogin() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://4478713d-fb5f-4722-8cfd-4eb632de9a6a-00-1h88qsaph29vx.spock.replit.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val call = apiService.login(email, password)
+        call.enqueue(object : Callback<List<LoginResponse>> {
+            override fun onResponse(
+                call: Call<List<LoginResponse>>,
+                response: Response<List<LoginResponse>>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    val loginResponses = response.body()!!
+                    if (loginResponses.isNotEmpty()) {
+
+
+
+
+                        val sharedPreferences = getSharedPreferences("Dados", Context.MODE_PRIVATE)
+                        sharedPreferences.edit().apply {
+                            putInt("id", loginResponses.get(0).usuarioId)
+                            apply()
+                        }
+
+
+
+                        val intent = Intent(this@MainActivity, ListaProdutos::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                } else {
+                    Log.e(
+                        "LoginActivity",
+                        "Login failed: HTTP error code: " + response.code() + " msg: " + response.message()
+                    )
+                    Toast.makeText(this@MainActivity, "Login failed", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<LoginResponse>>, t: Throwable) {
+                Log.e("LoginActivity", "onFailure: " + t.message)
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_LONG).show()
+            }
+        })
+
+
+    }
+
+
+    interface ApiService {
+        @GET("login")
+        fun login(
+            @Query("usuario") usuario: String,
+            @Query("senha") senha: String
+        ): Call<List<LoginResponse>>
+    }
+}
